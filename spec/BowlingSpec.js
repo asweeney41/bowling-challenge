2aebd0b502b087b87b950b9136254103c9133d3d
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-11-20 12:15:15 +0000

    Can roll: Gutter game & All ones

---
+++ spec/BowlingSpec.js
@@ -0,0 +1,22 @@
+describe('Bowling Game', function(){
+
+  var game;
+  beforeEach(function() {
+    game = new BowlingGame();
+  });
+
+
+  it('can roll a gutter game', function () {
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+
+  it('can roll all ones', function() {
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
+  });
+});
